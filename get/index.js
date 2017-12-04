e83ab2def81df0f26dd8c799af22abc99a00b3a3
Phodal HUANG <h@phodal.com>
2017-12-04 16:02:05 +0800

    [T] create functions

---
+++ get/index.js
@@ -0,0 +1,14 @@
+module.exports.handler = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: '',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
