c6023636307f43fc74a9859651c55f103fa3d956
Phodal HUANG <h@phodal.com>
2017-12-04 16:12:46 +0800

    create basic funciton

--- create/index.js
+++ create/index.js
@@ -1,14 +1,50 @@
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 module.exports.handler = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: '',
-      input: event,
-    }),
+
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.text !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the todo item.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      data: data.data,
+      env: data.env,
+      component: component,
+      checked: false,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
   };
 
-  callback(null, response);
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the log item.',
+      });
+      return;
+    }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
 };
