758c331ed9daa09e05bcba5d308ff9e4bcbe611e
Phodal HUANG <h@phodal.com>
2017-12-06 14:38:06 +0800

    [T] add cors config

--- list/index.js
+++ list/index.js
@@ -26,6 +26,9 @@ module.exports.list = (event, context, callback) => {
 
     const response = {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+      },
       body: JSON.stringify(result.Items),
     };
     callback(null, response);
