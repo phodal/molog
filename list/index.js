1aef32005a4602762daaaa01daf0908788efd034
Fengda HUANG <h@phodal.com>
2017-12-06 08:09:09 +0800

    [LIST] create basic function

---
+++ list/index.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: process.env.DYNAMODB_TABLE,
+};
+
+module.exports.list = (event, context, callback) => {
+  console.log(params)
+  dynamoDb.scan(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todos.',
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
